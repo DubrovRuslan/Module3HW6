77ff194aa4e50165b33b3a535ce00efd3f6d7122
DubrovRuslan <dubrov.ruslan@gmail.com>
2023-07-10 21:58:54 +0300

    Done

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Module3HW7
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var messageBox = new MessageBox();
+            var tcs = new TaskCompletionSource();
+            var rand = new Random();
+            messageBox.Message += (State state) =>
+            {
+                if (state == State.Ok)
+                {
+                    Console.WriteLine("Ok - операция была подтверждена");
+                }
+                else
+                {
+                    Console.WriteLine("Ok - операция была подтверждена");
+                }
+
+                tcs.SetResult();
+            };
+            messageBox.Open();
+            tcs.Task.GetAwaiter().GetResult();
+        }
+    }
+}
